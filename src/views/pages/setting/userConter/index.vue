847bf82a5fe852c190671a53ea9260336dff7249
e <2441604825@qq.com>
2020-09-21 20:30:43 +0800

    表格功能完成

--- src/views/pages/setting/userConter/index.vue
+++ src/views/pages/setting/userConter/index.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="mianContainer">
-    
-    <search-group ref="searchForm" :initData="searchData" @search="getData"></search-group>
-    用户设置中心
+    <div class="search">
+      <search-group ref="searchForm" :initData="searchData" @search="getData"></search-group>
+    </div>
+    <div class="main_tab">
+        <el-table :data="tableData" border align="center">
+          <template v-for="item in tableColumn">
+            <el-table-column
+             :label="item.label"
+             :show-overflow-tooltip="true"
+             :key="item.prop"
+             :prop="item.prop"
+             :min-width="item.width"
+             :formatter="item.formatter"
+            />
+          </template>
+           <el-table-column
+            fixed="right"
+            label="操作"
+            width="100">
+            <template slot-scope="scope">
+              <el-button type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">编辑</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+    </div>
   </div>
 </template>
 
 <script>
 import searchData from './mixin/search'
-import tableData from './mixin/table'
+import tableColumn from './mixin/table'
 export default {
-  mixins:[searchData,tableData],
+  mixins:[searchData,tableColumn],
   data(){
     return{
-      
+      loading:false,
+      tableData:[
+        {codeId:'001',userName:'测试',age:20,address:'浙江省下城隼目科技',conPer:'小灰灰',work:'前端开发',roleName:'法师'},
+        {codeId:'002',userName:'测试',age:20,address:'浙江省下城隼目科技',conPer:'小灰灰',work:'前端开发',roleName:'刺客'}
+      ]
     }
   },
   components:{
@@ -31,5 +58,9 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+  .main_tab{
+    width: 100%;
+
+  }
 </style>
